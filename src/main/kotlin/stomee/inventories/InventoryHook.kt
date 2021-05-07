5dd53b0fcf65cc54e5bbd13ff20dcd9df6056fb8
LeoDog896 <LeoDog896@hotmail.com>
2021-05-07 11:40:07 -0400

    Allow delay and pickup of items

--- src/main/kotlin/stomee/inventories/InventoryHook.kt
+++ src/main/kotlin/stomee/inventories/InventoryHook.kt
@@ -3,6 +3,8 @@ package stomee.inventories
 import net.minestom.server.entity.ItemEntity
 import net.minestom.server.entity.Player
 import net.minestom.server.event.item.ItemDropEvent
+import net.minestom.server.event.item.PickupItemEvent
+import net.minestom.server.utils.time.TimeUnit
 import world.cepi.kstom.addEventCallback
 
 fun hook(player: Player) {
@@ -10,5 +12,13 @@ fun hook(player: Player) {
         val entity = ItemEntity(itemStack, player.position, player.instance!!)
 
         entity.setInstance(player.instance!!, player.position)
+
+        entity.setPickupDelay(2, TimeUnit.SECOND)
+
+        entity.velocity.add(player.position.direction.clone().normalize().multiply(6))
+    }
+
+    player.addEventCallback<PickupItemEvent> {
+        player.inventory.addItemStack(itemEntity.itemStack)
     }
 }
